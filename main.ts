3a6240d0dc51940cd2b1daafacc08689c9ccd9b1
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2023-04-26 20:43:14 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    click += -1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(randint(0, click))
+})
+input.onButtonPressed(Button.B, function () {
+    click += 1
+})
+let click = 0
+click = 0
+let r = 0
+basic.forever(function () {
+    basic.showNumber(click)
+})

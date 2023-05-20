4c6ea1ebf43d027c73b6f4c8a18427c2bb799e40
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2023-05-20 08:41:23 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     click += -1
 })
+input.onGesture(Gesture.Shake, function () {
+    click = 0
+})
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(randint(0, click))
+    basic.pause(3000)
 })
 input.onButtonPressed(Button.B, function () {
     click += 1
